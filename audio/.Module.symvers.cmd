a645e7a102e5ea18ff835bfcac3d7e2e36135465
JayIke <jordineicher1@yahoo.com>
2024-04-13 16:07:17 -0500

    modified Makefile, driver, included install steps

---
+++ audio/.Module.symvers.cmd
@@ -0,0 +1,1 @@
+cmd_/home/jordin/rpi-webcam-gadget/audio/Module.symvers :=  sed 's/ko$$/o/'  /home/jordin/rpi-webcam-gadget/audio/modules.order | scripts/mod/modpost -m -a    -o /home/jordin/rpi-webcam-gadget/audio/Module.symvers -e -i Module.symvers -T - 
