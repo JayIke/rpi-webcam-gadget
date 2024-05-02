d20f6eab5e02be9fad73ea0be52401771d98c406
JayIke <jordineicher1@yahoo.com>
2024-05-02 00:49:40 -0500

    ah

--- custom.py
+++ custom.py
@@ -90,9 +90,7 @@ picam2 = Picamera2()
 config = picam2.create_preview_configuration(main={"size": (640, 480)})
 #config = picam2.create_preview_configuration(main={"size": (640, 480)},lores={"size": (320, 240), "format": "YUV420"})
 picam2.configure(config)
-(w0, h0) = picam2.stream_configuration("main")["size"]
-(w1, h1) = picam2.stream_configuration("lores")["size"]
-s1 = picam2.stream_configuration("lores")["stride"]
+
 picam2.post_callback = draw_faces
 output = StreamingOutput()
 #encoder = H264Encoder()
